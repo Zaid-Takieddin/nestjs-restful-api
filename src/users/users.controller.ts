865c6c1fbdfda79eb86265509422047bdf849a95
Zaid-Takieddin <zaidtakieddin@gmail.com>
2024-10-06 13:57:29 +0300

    initial commit

---
+++ src/users/users.controller.ts
@@ -0,0 +1,31 @@
+import { Body, Controller, Delete, Get, Param, Patch, Post, Query } from '@nestjs/common';
+
+@Controller('users')
+export class UsersController {
+
+  @Get()
+  findAll(@Query('role') role?: 'admin' | 'intern'){
+    return []
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id:string){
+    return {id}
+  }
+
+  @Post()
+  create(@Body() user: {}){
+    return user
+  }
+
+  @Patch(':id')
+  update(@Param('id') id:string, @Body() user:{}){
+    return {id,...user}
+  }
+
+  @Delete(':id')
+  delete(@Param('id') id:string){
+    return {id}
+  }
+
+}
